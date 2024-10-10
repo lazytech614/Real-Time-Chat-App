4f11735e97451518a74e97901be517a52ec5299d
lazytech614 <derupanjan2021@gmail.com>
2024-10-10 15:19:22 +0530

    update

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(()=> {
         if(authUser){
-            const socket = io("http://localhost:3000", {
+            const socket = io(`${import.meta.env.VITE_SERVER_URL}`, {
                 query: {
                     userId: authUser._id
                 }
